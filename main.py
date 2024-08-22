8053c60ea0e3c883e76353fd4d101736e7f69fc6
javier_prado <javier.1392@gmail.com>
2024-08-22 16:40:14 +0200

    add count words to main py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    n_words = count_words(text)
+    print(n_words)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(text : str):
+    words = text.split()
+    return len(words)
 
 main()

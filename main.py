8d56e47656bf5bbc3d25a30148ac890d3cf449a2
javier_prado <javier.1392@gmail.com>
2024-08-22 17:43:27 +0200

    Add chars dic to main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    n_words = count_words(text)
-    print(n_words)
+    num_words = get_num_words(text)
+    chars_dic = get_chars_dic(text)
+    print(chars_dic)
+   # print(num_words)
+
+
+def get_chars_dic(text: str):
+    chars = {}
+    for char in text:
+        if char.isalpha():
+            lowered = char.lower()
+            if lowered in chars:
+                chars[lowered] += 1
+            else:
+               chars[lowered] = 1 
+    return chars
+
+
+def get_num_words(text : str):
+    words = text.split()
+    return len(words)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(text : str):
-    words = text.split()
-    return len(words)
 
 main()

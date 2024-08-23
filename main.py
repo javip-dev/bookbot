adb2c0f39dbd2bef65d4d65e5e26838c7469f955
javier_prado <javier.1392@gmail.com>
2024-08-23 08:47:30 +0200

    Add print report

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dic = get_chars_dic(text)
-    print(chars_dic)
-   # print(num_words)
+    print_report(book_path,num_words,chars_dic)
 
 
 def get_chars_dic(text: str):
@@ -17,6 +16,27 @@ def get_chars_dic(text: str):
             else:
                chars[lowered] = 1 
     return chars
+
+
+def print_report(path , num_words, chars_dic: dict):
+    print(f"-- Begin report of {path}")
+    print(f"{num_words} found in the document")
+    print("\n")
+    list_from_dict = list(chars_dic.items())
+    list_chars_dic = []
+    for tuple in list_from_dict:
+        dic_temp = {}
+        dic_temp["letter"] = tuple[0]
+        dic_temp["num"] = tuple[1]
+        list_chars_dic.append(dic_temp)
+    list_chars_dic.sort(reverse=True, key=sort_on)
+    for l in list_chars_dic:
+        print(f"The '{l['letter']}' character was found {l['num']} times")
+    print("--- End report ---")
+    
+
+def sort_on(dict):
+    return dict["num"]
 
 
 def get_num_words(text : str):
